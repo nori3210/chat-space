1906470ed19a44270b525d2d1ee9c3f8038dd153
nori3210 <aiueoka3210@gmail.com>
2020-08-02 13:14:17 +0900

    create pull request

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   
+  devise_for :users
+  # 下の行は削除する
+  # get ''messages/index
+
  root "messages#index"
+ resources :users, only: [:edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
