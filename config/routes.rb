07d0e2ea1ab958b14393bcfccf837fc41fdb8b13
nori3210 <aiueoka3210@gmail.com>
2020-08-06 18:37:47 +0900

    グループの新規登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   
   devise_for :users
   # 下の行は削除する
-  # get ''messages/index
+  # get 'messages/index7
 
- root "messages#index"
- resources :users, only: [:edit, :update]
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
