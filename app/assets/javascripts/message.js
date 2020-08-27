d4ac3359bd72ef115a53fa9d4eb013ca8a8939e8
nori3210 <aiueoka3210@gmail.com>
2020-08-27 17:37:34 +0900

    メッセージ送信機能の非同期化

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,66 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="message-box">
+          <div class="message-info">
+            <div class="message-info__user-name">
+              ${message.user_name}
+            </div>
+            <div class="message-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message">
+            <p class="message__content">
+              ${message.content}
+            </p>
+            <img class="message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="message-box">
+        <div class="message-info">
+          <div class="message-info__user-name">
+            ${message.user_name}
+          </div>
+          <div class="message-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.form').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.chat-main__message-list').append(html);      
+      $('form')[0].reset();
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $('.submit-btn').prop('disabled', false);
+    })  
+    .fail(function() {
+          alert("メッセージ送信に失敗しました");
+    })
+  });
+});
